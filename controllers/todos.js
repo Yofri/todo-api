e1f5f90b7acaa0562598a7079c269c37d3da1ee9
Yofriadi Yahya <yofriadiyahya@gmail.com>
2017-12-16 16:16:41 +0700

    CRUD finish

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,7 +1,10 @@
 import Todo from '../models/todos'
-import mongoose from 'mongoose'
 import _ from 'lodash'
 
+/*
+ * @endpoint /api/todos
+ * @return json
+ */
 const create = async (req, res) => {
   try {
     console.log(req.body)
@@ -12,6 +15,10 @@ const create = async (req, res) => {
   }
 }
 
+/*
+ * @endpoint /api/todos
+ * @return json
+ */
 const lists = async (req, res) => {
   try {
     const todos = await Todo.find()
@@ -21,4 +28,39 @@ const lists = async (req, res) => {
   }
 }
 
-module.exports = {create, lists}
+/*
+ * @endpoint /api/todos
+ * @return json
+ */
+const find = async (req, res) => {
+  try {
+    const todo = await Todo.find({_id: req.params.id})
+    res.status(200).send(todo)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+/*
+ * @endpoint /api/todos
+ * @return json
+ */
+const update = async (req, res) => {
+  try {
+    const todo = await Todo.update({_id: req.params.id}, req.body)
+    res.status(200).send(todo)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const remove = async (req, res) => {
+  try {
+    const todo = await Todo.remove({_id: req.params.id})
+    res.status(200).send(todo)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+module.exports = { create, lists, find, update, remove}
--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,10 @@
 import express from 'express'
-import {create, lists} from '../controllers/todos'
+import {create, lists, find, update, remove} from '../controllers/todos'
 
 const router = express.Router()
 module.exports = router
   .post('/', create)
   .get('/', lists)
+  .get('/:id', find)
+  .put('/:id', update)
+  .delete('/:id', remove)
