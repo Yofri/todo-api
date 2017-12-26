bac8a82fc0adb11aa3252fdb46e616691eaeead2
Yofriadi Yahya <yofriadiyahya@gmail.com>
2017-12-26 18:01:51 +0700

    Preparing for deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-// const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const graphqlHTTP = require('express-graphql')
 // const cors = require('cors')
@@ -13,17 +12,15 @@ const db = mongoose.connection
 const port = process.env.PORT || 3000
 const uri = `mongodb://root:${process.env.DB_KEY}@todo-shard-00-00-vioh2.mongodb.net:27017,todo-shard-00-01-vioh2.mongodb.net:27017,todo-shard-00-02-vioh2.mongodb.net:27017/test?ssl=true&replicaSet=Todo-shard-0&authSource=admin`
 
-// mongoose.connect(uri, {useMongoClient: true})
-mongoose.connect('mongodb://localhost:27017/todo', {useMongoClient: true})
+mongoose.connect(uri, {useMongoClient: true})
+// mongoose.connect('mongodb://localhost:27017/todo', {useMongoClient: true})
 mongoose.Promise = global.Promise
 
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(bodyParser.json())
 // app.use(cors())
-// app.use(morgan('dev'))
 
 app.use('/todo', morgan('dev'), graphqlHTTP({
   schema,
+  pretty: true,
   graphiql: true
 }))
 
