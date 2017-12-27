c69e6f9af40da89dbc5fbc87de84826cc62ec9b3
Yofriadi Yahya <yofriadiyahya@gmail.com>
2017-12-27 09:25:57 +0700

    Use cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const graphqlHTTP = require('express-graphql')
-// const cors = require('cors')
+const cors = require('cors')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const schema = require('./controllers')
@@ -13,12 +13,9 @@ const port = process.env.PORT || 3000
 const uri = `mongodb://root:${process.env.DB_KEY}@todo-shard-00-00-vioh2.mongodb.net:27017,todo-shard-00-01-vioh2.mongodb.net:27017,todo-shard-00-02-vioh2.mongodb.net:27017/test?ssl=true&replicaSet=Todo-shard-0&authSource=admin`
 
 mongoose.connect(uri, {useMongoClient: true})
-// mongoose.connect('mongodb://localhost:27017/todo', {useMongoClient: true})
 mongoose.Promise = global.Promise
 
-// app.use(cors())
-
-app.use('/todo', morgan('dev'), graphqlHTTP({
+app.use('/todo', cors(), morgan('dev'), graphqlHTTP({
   schema,
   pretty: true,
   graphiql: true
